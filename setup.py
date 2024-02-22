f49cc72554e38abd7aa3a3b88749b0439b376174
Shubham Papal <shubhampapal13@gmail.com>
2024-02-23 01:08:32 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version="0.0.0",
-    author="Shubham",
+    author="shubham",
     author_email="shubhampapal13@gmail.com",
     packages=find_packages()
 )
